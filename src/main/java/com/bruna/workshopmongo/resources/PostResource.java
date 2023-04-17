d9cba5351f14986a7b1a082e310be0f1c245cfef
Bruna <brunasilv126@gmail.com>
2023-04-17 15:35:13 -0300

    consulta simples com query methods

--- src/main/java/com/bruna/workshopmongo/resources/PostResource.java
+++ src/main/java/com/bruna/workshopmongo/resources/PostResource.java
@@ -10,12 +10,14 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.bruna.workshopmongo.domain.Post;
 import com.bruna.workshopmongo.domain.User;
 import com.bruna.workshopmongo.dto.UserDTO;
+import com.bruna.workshopmongo.resources.util.URL;
 import com.bruna.workshopmongo.services.PostService;
 import com.bruna.workshopmongo.services.PostService;
 
@@ -32,4 +34,12 @@ public class PostResource {
 		Post obj = service.findById(id);
 		return ResponseEntity.ok().body(obj);
 	}
+	
+	@RequestMapping(value="/titlesearch", method=RequestMethod.GET)
+	public  ResponseEntity<List<Post>> findByTitle(@RequestParam(value="text", defaultValue = "") String text) {
+		text = URL.decodeParam(text);
+		List<Post> list = service.findByTitle(text);
+		return ResponseEntity.ok().body(list);
+	}
+	
 }
--- src/main/java/com/bruna/workshopmongo/services/PostService.java
+++ src/main/java/com/bruna/workshopmongo/services/PostService.java
@@ -1,5 +1,7 @@
 package com.bruna.workshopmongo.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -21,6 +23,10 @@ public class PostService {
 			throw new ObjectNotFoundException("Objto não encontrado");
 		}
 		return user;
+	}
+	
+	public List<Post> findByTitle(String text) {
+		return repo.findByTitleContainsIgnoreCase(text);
 	}
 	
 }	
